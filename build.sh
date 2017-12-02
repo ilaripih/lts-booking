725ae934e9a40e9859aa46dd1687077ed3431d8a
Ilari Pihlajisto <ilari.pihlajisto@vionice.fi>
2017-12-02 17:53:49 +0200

    remove the LTS dependency (issue #7)

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -x
 set -e
 
+ln -s $(pwd)/app/images_default app/images
 cd app
 polymer build --preset es5-bundled
 mv build/es5-bundled build/app
--- dev.sh
+++ dev.sh
@@ -3,5 +3,6 @@
 set -x
 set -e
 
+ln -s $(pwd)/app/images_default app/images
 docker run --rm -v $PWD:/app lts-booking
 LTS_BOOKING_PORT=8081 ./lts-booking
